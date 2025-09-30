047e87e26c42aac8d3a1900e4a1238aa74dadbd7
Dave Mateer <davemateer@gmail.com>
2025-09-30 14:34:53 +0100

    SSG for root home

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@
 // sfc - Stateless Function Component
 // notice this isn't async. only use async when need to to avoid double page request
 const Homepage = () => {
-  return <>Homepage - Minimal-nextjs</>;
+  const currentTime = new Date().toLocaleString();
+
+  return <>Homepage - Minimal-nextjs - build time is (ie this page is statically generated on the server - Server Side Generated) {currentTime} </>;
 };
 
 export default Homepage;
