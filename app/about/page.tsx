e1058e1d8ba42fd419b15a59caa042187d111b0f
Dave Mateer <davemateer@gmail.com>
2025-09-30 15:01:07 +0100

    SSR

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,20 @@
 // app/about/page.tsx
-const AboutPage = () => {
-  return <>About page</>;
+
+// this page is server side rendered on each request
+// see https://nextjs.org/docs/app/building-your-application/routing#dynamic-segments
+// the default is 'auto' which means next.js will decide based on if you use async or not
+// here we force it to be dynamic (server side rendered on each request) even though this component isn't async
+// this is just for demo purposes. normally you'd only use dynamic when you need to
+export const dynamic = 'force-dynamic';
+
+const AboutPage = async () => {
+  const currentTime = new Date().toLocaleString();
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl mb-4">About</h1>
+      <p>build time is (ie this page is Server Side Rendered on each request {currentTime}</p>
+    </div>
+  );
 };
 
 export default AboutPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
 // app/page.tsx
 // sfc - Stateless Function Component
-// notice this isn't async. only use async when need to to avoid double page request
+
+// notice this isn't async. only use async when need SSR
+// this page uses the default of 'auto' which means next.js will decide based on if you use async or not
+// see https://nextjs.org/docs/app/building-your-application/routing#dynamic-segments
+// here we explicitly set it to 'auto' just for demo purposes. normally you'd just leave this out
+
+// https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
+
+
+// keep as a statically generated page on the server.. not any good for dynamic data
+export const dynamic = 'auto';
+
 const Homepage = () => {
   const currentTime = new Date().toLocaleString();
 
   return (
   <div className="p-4">
     <h1 className="text-2xl mb-4">Home</h1>
-    <p>build time is (ie this page is statically generated on the server - Server Side Generated) {currentTime}</p>
+    <p>build time is (ie this page is the next.js default of statically generated on the server - Server Side Generated) {currentTime}</p>
   </div>
 );
 };
