de9c12e9f3826f278d4afbb09b9012a3d9dc8e92
Dave Mateer <davemateer@gmail.com>
2025-09-30 16:33:33 +0100

    sta

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -4,7 +4,7 @@ const AboutPage = async () => {
   const currentTime = new Date().toLocaleString();
   return (
     <div className="p-4">
-      <h1 className="text-2xl mb-4">Client Component</h1>
+      <h1 className="text-2xl mb-4">Client (static)</h1>
       <p>build time is (ie page data is Server Side Rendered on each request {currentTime}</p>
     </div>
   );
