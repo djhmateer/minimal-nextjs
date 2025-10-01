b56219076cecd37241ec2b28df7a7059348569fb
Dave Mateer <davemateer@gmail.com>
2025-10-01 13:03:34 +0100

    loading state

---
+++ app/users/loading.tsx
@@ -0,0 +1,8 @@
+export default function LoadingState() {
+    return (
+        <div className="p-4">
+            <h1 className="text-2xl mb-4">Loading users...</h1>
+            <p>Please wait while we fetch the user data.</p>
+        </div>
+    );
+}
