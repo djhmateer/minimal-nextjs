99e39e767e9d87a2a2d75762fafda31c453a3f3c
Dave Mateer <davemateer@gmail.com>
2025-10-01 05:56:13 +0100

    make call for posts

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,13 +3,18 @@
 export const dynamic = 'force-dynamic';
 
 const Post = async () => {
-  console.log('Post page');
-//   const currentTime = new Date().toLocaleString();
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl mb-4">Post</h1>
-    </div>
-  );
+    console.log('Posts page');
+    // const currentTime = new Date().toLocaleString();
+    // get some data https://jsonplaceholder.typicode.com/posts
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await response.json();
+    console.log('Posts fetched:', posts);
+
+    return (
+        <div className="p-4">
+            <h1 className="text-2xl mb-4">Post</h1>
+        </div>
+    );
 };
 
 export default Post;
