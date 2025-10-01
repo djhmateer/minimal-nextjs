6fd4b47d6936d4df68847e13ee5fa9c9d4e4fc3c
Dave Mateer <davemateer@gmail.com>
2025-10-01 06:37:09 +0100

    style button

--- app/contact/button.tsx
+++ app/contact/button.tsx
@@ -1,5 +1,5 @@
 "use client";
 
 export default function ButtonComponent() {
-    return <button onClick={() => alert("hello")}>Click Me</button>;
+    return <button onClick={() => alert("hello")} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">Click Me</button>;
 }
