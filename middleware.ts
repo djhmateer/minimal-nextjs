229ee0de1eea5e3426dab37f4c4384834a172416
Dave Mateer <davemateer@gmail.com>
2025-10-08 11:20:56 +0100

    Remove unused code and comments from middleware function for clarity

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,6 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  // const start = Date.now()
-
   const response = NextResponse.next()
 
   console.log(`${new Date().toISOString()} - ${request.method} ${request.nextUrl.pathname}`)
@@ -11,11 +9,6 @@ export function middleware(request: NextRequest) {
   return response
 }
 
-// defines which paths will invoke the middleware
-//   - /(...) - Match paths starting with /
-//   - (?!_next/static|_next/image|favicon.ico) - Negative lookahead: Don't match these patterns
-// also svgs
-//   - .* - Match everything else
 export const config = {
   matcher: '/((?!_next/static|_next/image|favicon.ico|.*\\.svg).*)',
 }
