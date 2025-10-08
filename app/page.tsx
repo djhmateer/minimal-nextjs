83686af741a332b08faea9e946f7960082c154ef
Dave Mateer <davemateer@gmail.com>
2025-10-08 12:12:00 +0100

    Add explanation of rendering combinations to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -29,6 +29,15 @@ export default function Home() {
       <h1 className="text-2xl mb-4">Home</h1>
       <p>Next.js default of statically generated on the server - Server Side Generated. Build time: {currentTime}</p>
       <p>There is a console.log(&quot;Home Page&quot;) which is only shown at build time. not shown on live prod (but is on dev as a server output as dev is built each time and browser console). Makes sense as a static</p>
+
+      <div>
+        <p>There are these combinations</p>
+        <ul>
+          <li>Static Site Generatation SSG (this page). No `export const dynamic = 'force-dynamic'`. console.log will show only in initial build output. It is just a html page. no custom js to run on client.</li>
+          <li>SSG with `use client` - have access to the browser. Classic React style. console.log to client. </li>
+          <li>Server Side Render SSR with `export const dynamic = 'force-dynamic'` - renders on server each time. console.log to server.</li>
+        </ul>
+      </div>
     </div>
   );
 }
