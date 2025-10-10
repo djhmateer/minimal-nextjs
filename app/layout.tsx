9e435520b5df93d2cb18d1e0473696b5765a5c02
Dave Mateer <davemateer@gmail.com>
2025-10-10 06:37:55 +0100

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,9 +63,10 @@ export default function RootLayout({
                 {/* <Link href="/foo" className="text-gray-600 hover:text-blue-600 transition-colors" prefetch={false}>404</Link> */}
                 <Link href="/simpleform" className="text-gray-600 hover:text-blue-600 transition-colors" prefetch={false}>1 and 3.simpleform</Link>
                 <Link href="/simpleformclient" className="text-gray-600 hover:text-blue-600 transition-colors" prefetch={false}>1, 3 and 4.simpleformclient</Link>
-                <Link href="/shadcn" className="text-gray-600 hover:text-blue-600 transition-colors" prefetch={false}>2.shadcn</Link>
+                {/* <Link href="/shadcn" className="text-gray-600 hover:text-blue-600 transition-colors" prefetch={false}>2.shadcn</Link> */}
                 <Link href="/shadcnb" className="text-gray-600 hover:text-blue-600 transition-colors" prefetch={false}>1. 3 and 4.shadcnb</Link>
                 <Link href="/shadcnzod" className="text-gray-600 hover:text-blue-600 transition-colors" prefetch={false}>1. 3 and 4.shadcnzod</Link>
+                <Link href="/shadcngrid" className="text-gray-600 hover:text-blue-600 transition-colors" prefetch={false}>1. 3 and 4.shadcngrid</Link>
               </div>
             </nav>
           </div>
